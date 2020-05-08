5fb36a2bf78f82fdb1ef16912bb6e55879603138
Sean Mackey <sean@appiphony.com>
2020-05-08 12:06:17 -0500

    creating lightning component for cleaning tracking; laying out initial structure

---
+++ force-app/main/default/lwc/locationCleaning/locationCleaning.js
@@ -0,0 +1,13 @@
+import { LightningElement } from 'lwc';
+
+export default class LocationCleaning extends LightningElement {
+    /// DEMO STATES - refactor or remove these ///
+    noLocationView = false;
+    locationView = true;
+    subLocationView = true;
+    /// END DEMO STATES ///
+
+    /// DEMO DATA - rafactor or remove this ///
+
+    /// END DEMO DATA ///
+}
